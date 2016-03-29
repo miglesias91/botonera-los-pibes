ce42398e6548bbb0a0d3eda93646c9b7baad0397
manuel <m.iglesias91@gmail.com>
2016-03-28 23:47:40 -0300

    refactorizacion. 1 solo soundpools para el manejo de sonidos.

--- app/src/main/java/com/example/manuel/botoneralospibes/SetSonidos.java
+++ app/src/main/java/com/example/manuel/botoneralospibes/SetSonidos.java
@@ -14,10 +14,7 @@ import java.util.HashMap;
  */
 public class SetSonidos {
 
-    private ArrayList<SoundPool> sound_pools;
-    private SoundPool sound_pool_activo;
-    private int idx_sound_pool_activo = 0;
-    private int num_sound_pools = 2;
+    private SoundPool sound_pool;
 
     private HashMap<Integer, Integer> mapa_sonidos;
     int priority = 1;
@@ -27,8 +24,8 @@ public class SetSonidos {
 
     private Context context;
 
-    private static int ultimo_id_reproducido = 0;
-    private static int id_play = 0;
+    private static int ultimo_id_reproducido = -1;
+    private static Integer id_play;
 
     public static final int SONIDO_FRANCO_DESPERTATE_HDP = 1;
     public static final int SONIDO_FRANCO_HE_GUACHO = 2;
@@ -53,9 +50,6 @@ public class SetSonidos {
     public void init( Context context ) {
         this.context = context;
 
-        sound_pools = new ArrayList<SoundPool>(num_sound_pools);
-
-        Log.i( "SetSonidos", "Numero de SoundPools = " + num_sound_pools );
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
 
             AudioAttributes audio_atrib = new AudioAttributes.Builder()
@@ -63,80 +57,46 @@ public class SetSonidos {
                     .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
                     .build();
 
-            //for ( SoundPool s : sound_pools ) {
-            //    s = new SoundPool.Builder().setAudioAttributes(audio_atrib).build();
-            //}
-            for ( int i = 0 ; i < num_sound_pools ; i++ ) {
-                sound_pools.add( new SoundPool.Builder().setAudioAttributes(audio_atrib).build() );
-            }
-            //sound_pool = new SoundPool.Builder().setAudioAttributes(audio_atrib).build();
+            sound_pool = new SoundPool.Builder().setAudioAttributes(audio_atrib).build();
             Log.i( "SetSonidos", "Iniciado con API >= 21" );
         }
         else {
-            //for ( SoundPool s : sound_pools ) {
-            //    s = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
-            //}
-            for ( int i = 0 ; i < num_sound_pools ; i++ ) {
-                sound_pools.add( new SoundPool(10, AudioManager.STREAM_MUSIC, 0) );
-            }
-            //sound_pool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+            sound_pool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
             Log.i( "SetSonidos", "Iniciado con API < 21" );
         }
 
         mapa_sonidos = new HashMap<Integer, Integer>();
 
-        for ( SoundPool s : sound_pools ) {
-            // audios franco
-            mapa_sonidos.put(SONIDO_FRANCO_DESPERTATE_HDP, s.load(context, R.raw.franco_despertate_hdp, 1));
-            mapa_sonidos.put(SONIDO_FRANCO_HE_GUACHO, s.load(context, R.raw.franco_he_guacho, 1));
-            mapa_sonidos.put(SONIDO_FRANCO_MADRID_PAPA, s.load(context, R.raw.franco_madrid_papa, 1));
-            mapa_sonidos.put(SONIDO_FRANCO_RE_VA, s.load(context, R.raw.franco_re_va, 1));
-            mapa_sonidos.put(SONIDO_FRANCO_REZARPADO_RELOCO, s.load(context, R.raw.franco_rezarpado_reloco, 1));
+        // audios franco
+        mapa_sonidos.put(SONIDO_FRANCO_DESPERTATE_HDP, sound_pool.load(context, R.raw.franco_despertate_hdp, 1));
+        mapa_sonidos.put(SONIDO_FRANCO_HE_GUACHO, sound_pool.load(context, R.raw.franco_he_guacho, 1));
+        mapa_sonidos.put(SONIDO_FRANCO_MADRID_PAPA, sound_pool.load(context, R.raw.franco_madrid_papa, 1));
+        mapa_sonidos.put(SONIDO_FRANCO_RE_VA, sound_pool.load(context, R.raw.franco_re_va, 1));
+        mapa_sonidos.put(SONIDO_FRANCO_REZARPADO_RELOCO, sound_pool.load(context, R.raw.franco_rezarpado_reloco, 1));
 
-            // audios jm
-            mapa_sonidos.put(SONIDO_JM_GOING_CRAZY, s.load(context, R.raw.jm_going_crazy, 1));
-            mapa_sonidos.put(SONIDO_JM_JOJOJO, s.load(context, R.raw.jm_jojojo, 1));
-            mapa_sonidos.put(SONIDO_JM_OSITO_DORMILION, s.load(context, R.raw.jm_osito_dormilon, 1));
-        }
+        // audios jm
+        mapa_sonidos.put(SONIDO_JM_GOING_CRAZY, sound_pool.load(context, R.raw.jm_going_crazy, 1));
+        mapa_sonidos.put(SONIDO_JM_JOJOJO, sound_pool.load(context, R.raw.jm_jojojo, 1));
+        mapa_sonidos.put(SONIDO_JM_OSITO_DORMILION, sound_pool.load(context, R.raw.jm_osito_dormilon, 1));
 
         AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         volume = audioManager.getStreamVolume(AudioManager.STREAM_SYSTEM);
-
-        sound_pool_activo = sound_pools.get( idx_sound_pool_activo );
     }
 
     public void reproducir(int id_sonido_a_reproducir) {
 
-        if ( ultimo_id_reproducido == id_sonido_a_reproducir ) {
-            //this.cambiarSoundPool();
-            //this.sound_pool_activo.pause(this.id_play);
-            //this.sound_pool_activo.resume(this.id_play);
-            //Log.i("SetSonidos", "Reproduciendo sonido: " + id_sonido_a_reproducir);
-            //Log.i("SetSonidos", "id_play: " + this.id_play);
-            //return;
+        if ( this.ultimo_id_reproducido == id_sonido_a_reproducir ) {
+            int aux = id_sonido_a_reproducir - 1;
+            if ( aux == 0 ) {
+                aux += 3;
+            }
+            this.id_play = this.sound_pool.play(mapa_sonidos.get(aux), volume, volume, priority, no_loop, normal_playback_rate);
+            this.sound_pool.stop(this.id_play);
         }
 
-        ultimo_id_reproducido = id_sonido_a_reproducir;
-        this.sound_pool_activo.stop(this.id_play);
-        this.id_play = this.sound_pool_activo.play(mapa_sonidos.get(id_sonido_a_reproducir), volume, volume, priority, no_loop, normal_playback_rate);
+        this.ultimo_id_reproducido = id_sonido_a_reproducir;
+        this.id_play = this.sound_pool.play(mapa_sonidos.get(id_sonido_a_reproducir), volume, volume, priority, no_loop, normal_playback_rate);
         Log.i("SetSonidos", "Reproduciendo sonido: " + id_sonido_a_reproducir);
-        Log.i("SetSonidos", "id_play: " + this.id_play);
-    }
-
-    public void cambiarSoundPool() {
-        // paro el soundpool q se esta reproduciendo.
-        this.id_play = this.sound_pool_activo.play(mapa_sonidos.get(ultimo_id_reproducido--), volume, volume, priority, no_loop, normal_playback_rate);
-        this.sound_pool_activo.stop(this.id_play);
-
-        if ( 0 == this.idx_sound_pool_activo ) {
-            this.idx_sound_pool_activo = 1;
-        } else {
-            this.idx_sound_pool_activo = 0;
-        }
-        // cambio de soundpool
-        //this.sound_pool_activo = this.sound_pools.get( this.idx_sound_pool_activo );
-
-        Log.i("SetSonidos", "Idx SoundPool activo: " + this.idx_sound_pool_activo);
         Log.i("SetSonidos", "id_play: " + this.id_play);
     }
 }
