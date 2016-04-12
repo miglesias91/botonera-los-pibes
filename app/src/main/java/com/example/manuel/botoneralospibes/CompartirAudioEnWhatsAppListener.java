f7223b530a14a8f3061524fd786963de4df63a11
manuel <m.iglesias91@gmail.com>
2016-04-11 21:49:45 -0300

    integracion con whatsapp + ajuste de volumen. close #7 close #1

--- app/src/main/java/com/example/manuel/botoneralospibes/CompartirAudioEnWhatsAppListener.java
+++ app/src/main/java/com/example/manuel/botoneralospibes/CompartirAudioEnWhatsAppListener.java
@@ -17,6 +17,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
 
 /**
  * Created by Manuel on 30/03/2016.
@@ -24,16 +25,41 @@ import java.io.OutputStream;
 public class CompartirAudioEnWhatsAppListener implements View.OnClickListener {
 
     Context context;
+    Integer id_raw;
 
-    public CompartirAudioEnWhatsAppListener(Context context) {
+    private static final int SONIDO_FRANCO_DESPERTATE_HDP = 1;
+    private static final int SONIDO_FRANCO_HE_GUACHO = 2;
+    private static final int SONIDO_FRANCO_MADRID_PAPA = 3;
+    private static final int SONIDO_FRANCO_RE_VA = 4;
+    private static final int SONIDO_FRANCO_REZARPADO_RELOCO = 5;
+    private static final int SONIDO_JM_GOING_CRAZY = 6;
+    private static final int SONIDO_JM_JOJOJO = 7;
+    private static final int SONIDO_JM_OSITO_DORMILION = 8;
+
+    private static HashMap<Integer, Integer> mapa_nombres = new HashMap<Integer, Integer>();
+
+    public CompartirAudioEnWhatsAppListener(Context context, Integer id_raw) {
         this.context = context;
+        this.id_raw = id_raw;
+    }
+
+    public static void init() {
+        mapa_nombres.put(SONIDO_FRANCO_DESPERTATE_HDP,  R.raw.franco_despertate_hdp_wa);
+        mapa_nombres.put(SONIDO_FRANCO_HE_GUACHO,  R.raw.franco_he_guacho_wa);
+        mapa_nombres.put(SONIDO_FRANCO_MADRID_PAPA,  R.raw.franco_madrid_papa_wa);
+        mapa_nombres.put(SONIDO_FRANCO_RE_VA,  R.raw.franco_re_va_wa);
+        mapa_nombres.put(SONIDO_FRANCO_REZARPADO_RELOCO,  R.raw.franco_rezarpado_reloco_wa);
+        mapa_nombres.put(SONIDO_JM_GOING_CRAZY,  R.raw.jm_going_crazy_wa);
+        mapa_nombres.put(SONIDO_JM_JOJOJO,  R.raw.jm_jojojo_wa);
+        mapa_nombres.put(SONIDO_JM_OSITO_DORMILION,  R.raw.jm_osito_dormilon_wa);
     }
 
     public void onClick( View view ) {
         Intent wa_intent = new Intent();
         wa_intent.setAction(Intent.ACTION_SEND);
-        wa_intent.putExtra(Intent.EXTRA_STREAM, Uri.parse("android.resource://com.example.manuel.botoneralospibes/" + R.raw.jm_jojojo_wa));
+        wa_intent.putExtra(Intent.EXTRA_STREAM, Uri.parse("android.resource://com.example.manuel.botoneralospibes/" + mapa_nombres.get(this.id_raw)));
         wa_intent.setType("audio/*");
-        view.getContext().startActivity(Intent.createChooser(wa_intent, "Share Sound File"));
+        wa_intent.setPackage("com.whatsapp");
+        view.getContext().startActivity(wa_intent);
     }
 }
--- app/src/main/java/com/example/manuel/botoneralospibes/MainActivity.java
+++ app/src/main/java/com/example/manuel/botoneralospibes/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.manuel.botoneralospibes;
 
+import android.content.Intent;
 import android.media.Image;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    // botones audio
     private ImageButton boton_franco_despertate_hdp;
     private ImageButton boton_franco_he_guacho;
     private ImageButton boton_franco_madrid_papa;
@@ -16,7 +20,16 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton boton_jm_going_crazy;
     private ImageButton boton_jm_jojojo;
     private ImageButton boton_jm_osito_dormilon;
-    private ImageButton boton_compartir_wa;
+
+    // botones whatsapp
+    private ImageButton boton_franco_despertate_hdp_compartir;
+    private ImageButton boton_franco_he_guacho_compartir;
+    private ImageButton boton_franco_madrid_papa_compartir;
+    private ImageButton boton_franco_re_va_compartir;
+    private ImageButton boton_franco_rezarpado_reloco_compartir;
+    private ImageButton boton_jm_going_crazy_compartir;
+    private ImageButton boton_jm_jojojo_compartir;
+    private ImageButton boton_jm_osito_dormilon_compartir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,32 +37,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SetSonidos.getInstance().init(this);
+        CompartirAudioEnWhatsAppListener.init();
 
         boton_franco_despertate_hdp = (ImageButton) findViewById( R.id.boton_franco_despertate_hdp );
         boton_franco_despertate_hdp.setOnClickListener(new ReproducirBotonListener(1));
 
+        boton_franco_despertate_hdp_compartir = (ImageButton) findViewById( R.id.boton_franco_despertate_hdp_compartir );
+        boton_franco_despertate_hdp_compartir.setOnClickListener(new CompartirAudioEnWhatsAppListener(this, 1));
+
         boton_franco_he_guacho = (ImageButton) findViewById( R.id.boton_franco_he_guacho );
         boton_franco_he_guacho.setOnClickListener(new ReproducirBotonListener(2));
+
+        boton_franco_he_guacho_compartir = (ImageButton) findViewById( R.id.boton_franco_he_guacho_compartir );
+        boton_franco_he_guacho_compartir.setOnClickListener(new CompartirAudioEnWhatsAppListener(this, 2));
 
         boton_franco_madrid_papa = (ImageButton) findViewById( R.id.boton_franco_madrid_papa );
         boton_franco_madrid_papa.setOnClickListener(new ReproducirBotonListener(3));
 
+        boton_franco_madrid_papa_compartir = (ImageButton) findViewById( R.id.boton_franco_madrid_papa_compartir );
+        boton_franco_madrid_papa_compartir.setOnClickListener(new CompartirAudioEnWhatsAppListener(this, 3));
+
         boton_franco_re_va = (ImageButton) findViewById( R.id.boton_franco_re_va );
         boton_franco_re_va.setOnClickListener(new ReproducirBotonListener(4));
+
+        boton_franco_re_va_compartir = (ImageButton) findViewById( R.id.boton_franco_re_va_compartir );
+        boton_franco_re_va_compartir.setOnClickListener(new CompartirAudioEnWhatsAppListener(this, 4));
 
         boton_franco_rezarpado_reloco = (ImageButton) findViewById( R.id.boton_franco_rezarpado_reloco );
         boton_franco_rezarpado_reloco.setOnClickListener(new ReproducirBotonListener(5));
 
+        boton_franco_rezarpado_reloco_compartir = (ImageButton) findViewById( R.id.boton_franco_rezarpado_reloco_compartir );
+        boton_franco_rezarpado_reloco_compartir.setOnClickListener(new CompartirAudioEnWhatsAppListener(this, 5));
+
         boton_jm_going_crazy = (ImageButton) findViewById( R.id.boton_jm_going_crazy );
         boton_jm_going_crazy.setOnClickListener(new ReproducirBotonListener(6));
+
+        boton_jm_going_crazy_compartir = (ImageButton) findViewById( R.id.boton_jm_going_crazy_compartir );
+        boton_jm_going_crazy_compartir.setOnClickListener(new CompartirAudioEnWhatsAppListener(this, 6));
 
         boton_jm_jojojo = (ImageButton) findViewById( R.id.boton_jm_jojojo );
         boton_jm_jojojo.setOnClickListener(new ReproducirBotonListener(7));
 
+        boton_jm_jojojo_compartir = (ImageButton) findViewById( R.id.boton_jm_jojojo_compartir );
+        boton_jm_jojojo_compartir.setOnClickListener(new CompartirAudioEnWhatsAppListener(this, 7));
+
         boton_jm_osito_dormilon = (ImageButton) findViewById( R.id.boton_jm_osito_dormilon );
         boton_jm_osito_dormilon.setOnClickListener(new ReproducirBotonListener(8));
 
-        boton_compartir_wa = (ImageButton) findViewById( R.id.boton_compartir_wa );
-        boton_compartir_wa.setOnClickListener( new CompartirAudioEnWhatsAppListener(this) );
+        boton_jm_osito_dormilon_compartir = (ImageButton) findViewById( R.id.boton_jm_osito_dormilon_compartir );
+        boton_jm_osito_dormilon_compartir.setOnClickListener(new CompartirAudioEnWhatsAppListener(this, 8));
     }
 }
